d6f09b9458f6dd6de320c28337e914eb0b133184
hamsterchain <enneftee@gmail.com>
2025-11-05 01:52:34 +0100

    Update index.tsx
    back to manual test

--- pages/index.tsx
+++ pages/index.tsx
@@ -26,13 +26,14 @@ const Home: NextPage = () => {
           //CHANGES WERE MADE HERE
           var beforeSanitized = info!.data.toString()
           let index = beforeSanitized.indexOf("data");
-          let indexend = beforeSanitized.indexOf('","attributes');
+
+          
+          //let indexend = beforeSanitized.indexOf('","attributes');
+          
           //let index = beforeSanitized.indexOf("");
-          try{
-          let valSanitised = beforeSanitized.substring(index,indexend);
-          }catch (error){
+          
+          
           let valSanitised = beforeSanitized.substring(index);
-          }
 
           setData(valSanitised);
         } catch (error){
